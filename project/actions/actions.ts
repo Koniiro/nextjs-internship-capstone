39af8965ade98d9a293d64df06fb9a5b2623c2dc
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-28 17:45:30 +0800

    chore(database): Fixed package source

--- project/actions/actions.ts
+++ project/actions/actions.ts
@@ -2,10 +2,29 @@
 "use server";
 import { neon } from "@neondatabase/serverless";
 import 'dotenv/config';
-import { db } from "@/db"
-import { usersTable } from "@/db/schema";
+import { db } from "@/lib/db"
+import { usersTable } from "@/lib/db/schema";
 
 
 export async function getUserData() {
      return  db.select().from(usersTable);
+}
+export const createUser=async (
+  nClerkId:string,
+  nUserName:string,
+  nEmail: string,
+  nCreatedAt: Date,
+
+)=>{
+  await db.insert(usersTable).values({
+     clerkId:nClerkId,
+     name:nUserName,
+     email:nEmail,
+     createdAt:nCreatedAt,
+     updatedAt:nCreatedAt
+
+  }).onConflictDoNothing()
+
+  return { success: true }
+
 }
