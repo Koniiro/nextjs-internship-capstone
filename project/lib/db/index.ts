0da5d2b34b1c9b7b7dc796aa35bf8b4795baf09d
ingonzales-strat <i_isaiahnikolo.gonzales@stratpoint.com>
2025-07-28 16:59:24 +0800

    fix(database): Schema location ifx

--- project/lib/db/index.ts
+++ project/lib/db/index.ts
@@ -40,7 +40,7 @@ export const queries = {
 
 import {config} from "dotenv";
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from '@/db/schema';
+import * as schema from '@/lib/db/schema';
 
 config({path:".env"});
 export const db = drizzle(process.env.DATABASE_URL!,{schema});
